b13b1671a46fa79119baf71f04d9f2b81cbdff1e
Hemanth Malla <hemanth.malla@datadoghq.com>
2024-07-25 16:21:27 -0400

    Add exp. backoff for kvstore connection retries
    Signed-off-by: Hemanth Malla <hemanth.malla@datadoghq.com>

--- pkg/kvstore/etcd.go
+++ pkg/kvstore/etcd.go
@@ -458,6 +458,7 @@ func (e *etcdClient) waitForInitLock(ctx context.Context) <-chan error {
 	initLockSucceeded := make(chan error)
 
 	go func() {
+		limiter := GetExpBackoffRateLimiter(e, "etcd-client-init-lock")
 		for {
 			select {
 			case <-e.client.Ctx().Done():
@@ -487,8 +488,9 @@ func (e *etcdClient) waitForInitLock(ctx context.Context) <-chan error {
 				return
 			}
 
-			time.Sleep(100 * time.Millisecond)
+			limiter.Wait(ctx)
 		}
+		limiter.Reset()
 	}()
 
 	return initLockSucceeded
@@ -534,11 +536,25 @@ func (e *etcdClient) isConnectedAndHasQuorum(ctx context.Context) error {
 	}
 }
 
+func GetExpBackoffRateLimiter(e *etcdClient, name string) backoff.Exponential {
+	errLimiter := backoff.Exponential{
+		Name: name,
+		Min:  50 * time.Millisecond,
+		Max:  1 * time.Minute,
+	}
+
+	if e != nil && e.extraOptions != nil {
+		errLimiter.NodeManager = backoff.NewNodeManager(e.extraOptions.ClusterSizeDependantInterval)
+	}
+	return errLimiter
+}
+
 // Connected closes the returned channel when the etcd client is connected. If
 // the context is cancelled or if the etcd client is closed, an error is
 // returned on the channel.
 func (e *etcdClient) Connected(ctx context.Context) <-chan error {
 	out := make(chan error)
+	limiter := GetExpBackoffRateLimiter(e, "etcd-client-connected")
 	go func() {
 		defer close(out)
 		for {
@@ -554,8 +570,9 @@ func (e *etcdClient) Connected(ctx context.Context) <-chan error {
 			if e.isConnectedAndHasQuorum(ctx) == nil {
 				return
 			}
-			time.Sleep(100 * time.Millisecond)
+			limiter.Wait(ctx)
 		}
+		limiter.Reset()
 	}()
 	return out
 }
