2e9c53f02a087072a35c0b01f3975cb2165a5035
Anton Ippolitov <anton.ippolitov@datadoghq.com>
2024-07-31 12:24:25 +0000

    kvstoremesh: add configuration options for logging
    Signed-off-by: Anton Ippolitov <anton.ippolitov@datadoghq.com>

--- clustermesh-apiserver/kvstoremesh/root.go
+++ clustermesh-apiserver/kvstoremesh/root.go
@@ -33,8 +33,8 @@ func NewCmd(h *hive.Hive) *cobra.Command {
 			// Overwrite the metrics namespace with the one specific for KVStoreMesh
 			metrics.Namespace = metrics.CiliumKVStoreMeshNamespace
 			option.Config.Populate(h.Viper())
-			if option.Config.Debug {
-				log.Logger.SetLevel(logrus.DebugLevel)
+			if err := logging.SetupLogging(option.Config.LogDriver, option.Config.LogOpt, "kvstoremesh", option.Config.Debug); err != nil {
+				log.Fatal(err)
 			}
 			option.LogRegisteredOptions(h.Viper(), log)
 		},
