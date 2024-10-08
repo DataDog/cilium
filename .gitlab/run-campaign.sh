66513fd7ad6e62f1db8ddbea461a10905000eb99
Anton Ippolitov <anton.ippolitov@datadoghq.com>
2024-10-08 11:55:22 +0200

    Fix branch

--- .gitlab/run-campaign.sh
+++ .gitlab/run-campaign.sh
@@ -2,9 +2,10 @@
 set -exuo pipefail
 
 # This is required for the set-head command to succeed
-git config --global --add safe.directory $CI_PROJECT_DIR
+git config --global --add safe.directory "$CI_PROJECT_DIR"
 # Set the branch where .campaigns.toml is located
-git remote set-head origin $CI_COMMIT_REF_NAME
+BRANCH=$(git branch --all --contains "$CI_COMMIT_TAG" --format='%(refname:short)')
+git remote set-head origin "$BRANCH"
 
 export CURRENT_DATE=$(date +"%Y-%m-%d")
 envsubst < .gitlab/campaign-template.yaml > campaign.yaml
