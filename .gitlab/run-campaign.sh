2653761d6e4dce2cf4b1ea30e621c704dceabace
Anton Ippolitov <anton.ippolitov@datadoghq.com>
2024-10-10 16:16:32 +0200

    Campaigner: build 1 tag per CI pipeline (#577)

--- .gitlab/run-campaign.sh
+++ .gitlab/run-campaign.sh
@@ -2,21 +2,17 @@
 set -exuo pipefail
 
 # This is required for the set-head command to succeed
-git config --global --add safe.directory $CI_PROJECT_DIR
+git config --global --add safe.directory "$CI_PROJECT_DIR"
 # Set the branch where .campaigns.toml is located
-git remote set-head origin $CI_COMMIT_REF_NAME
+BRANCH=$(git branch --all --contains "$CI_COMMIT_TAG" --format='%(refname:short)' | sed -n 's/^origin\///p')
+git remote set-head origin "$BRANCH"
 
 export CURRENT_DATE=$(date +"%Y-%m-%d")
+export IMAGE_TAG="$CI_COMMIT_TAG"
+TIMESTAMP=${CI_PIPELINE_CREATED_AT//:/-}
+TIMESTAMP=${TIMESTAMP,,}
+IMAGE_TAG="${IMAGE_TAG}-${TIMESTAMP}"
 envsubst < .gitlab/campaign-template.yaml > campaign.yaml
-
-# Generate search and replace rules for image tags
-# This will bump only existing image tags
-while IFS=' ' read -r GIT_TAG IMAGE_TAG; do
-  cat <<EOF >> campaign.yaml
-  - op: "custom/search_and_replace"
-    key: !perl/regexp "\"${GIT_TAG}[a-z0-9-]*\".*since.*"
-    value: "\"${IMAGE_TAG}\" # since ${CURRENT_DATE}"
-EOF
-done < image_tags.txt
+cat campaign.yaml
 
 campaigns start --env prod --config-file campaign.yaml
