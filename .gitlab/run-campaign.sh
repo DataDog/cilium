e6ec7080a271b5b10351f188e0014b4edeaf4f27
Anton Ippolitov <anton.ippolitov@datadoghq.com>
2024-10-24 14:53:18 +0200

    Campaigner CI: Fix logic to find the main branch (#580)

--- .gitlab/run-campaign.sh
+++ .gitlab/run-campaign.sh
@@ -3,8 +3,8 @@ set -exuo pipefail
 
 # This is required for the set-head command to succeed
 git config --global --add safe.directory "$CI_PROJECT_DIR"
-# Set the branch where .campaigns.toml is located
-BRANCH=$(git branch --all --contains "$CI_COMMIT_TAG" --format='%(refname:short)' | sed -n 's/^origin\///p')
+# Set the branch where .campaigns.toml is located (e.g. v1.15-dd)
+BRANCH=$(sed -E 's/^/v/; s/\.[[:digit:]]+$/-dd/g' VERSION)
 git remote set-head origin "$BRANCH"
 
 export CURRENT_DATE=$(date +"%Y-%m-%d")
