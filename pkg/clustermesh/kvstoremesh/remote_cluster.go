8707f641b6fe89a05049cf9547730213f260e369
Anton Ippolitov <anton.ippolitov@datadoghq.com>
2024-12-02 13:24:44 +0100

    Test syncstore prefix

--- pkg/clustermesh/kvstoremesh/remote_cluster.go
+++ pkg/clustermesh/kvstoremesh/remote_cluster.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"fmt"
 	"path"
+	"strings"
 	"sync"
 
 	"github.com/cilium/cilium/api/v1/models"
@@ -167,8 +168,12 @@ func (o *syncer) OnSync(ctx context.Context) {
 
 func newReflector(local kvstore.BackendOperations, cluster, prefix string, factory store.Factory) reflector {
 	prefix = kvstore.StateToCachePrefix(prefix)
+	var syncStorePrefix = prefix
+	if strings.Contains(prefix, "identities") {
+		syncStorePrefix = path.Join(prefix, cluster, "id")
+	}
 	syncer := syncer{
-		SyncStore: factory.NewSyncStore(cluster, local, path.Join(prefix, cluster),
+		SyncStore: factory.NewSyncStore(cluster, local, syncStorePrefix,
 			store.WSSWithSyncedKeyOverride(prefix)),
 	}
 
