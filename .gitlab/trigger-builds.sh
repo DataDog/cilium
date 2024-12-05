142333e9d33d01ed45440d77b151ff555e1f7fe9
Anton Ippolitov <anton.ippolitov@datadoghq.com>
2024-12-05 14:42:37 +0100

    Initial Gitlab setup for 1.16

---
+++ .gitlab/trigger-builds.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+
+# Find the 3 latest -dd tags on the current branch
+GIT_TAGS_TO_BUILD=$(git --no-pager tag --sort=-creatordate --merged HEAD --list \*-dd\* | head -n 3)
+
+# Trigger a CI pipeline for each tag
+for TAG in $GIT_TAGS_TO_BUILD; do
+  curl --request POST \
+       --form token="${CI_JOB_TOKEN}" \
+       --form ref="$TAG" \
+       "https://gitlab.ddbuild.io/api/v4/projects/${CI_PROJECT_ID}/trigger/pipeline"
+done
