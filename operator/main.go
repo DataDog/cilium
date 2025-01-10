df28fd86fccffc7dbadb3213d0c8d8fb220d1a3b
Hadrien Patte <hadrien.patte@datadoghq.com>
2025-01-10 14:26:11 +0100

    Add tracing
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

--- operator/main.go
+++ operator/main.go
@@ -6,9 +6,16 @@ package main
 import (
 	"github.com/cilium/cilium/operator/cmd"
 	"github.com/cilium/cilium/pkg/hive"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
 func main() {
+	tracer.Start(
+		tracer.WithEnv("experimental"),
+		tracer.WithService("cilium-operator"),
+	)
+	defer tracer.Stop()
+
 	operatorHive := hive.New(cmd.Operator)
 
 	cmd.Execute(cmd.NewOperatorCmd(operatorHive))
