847b3d5c274725c6ef99d49a7114a862574c0c04
Hadrien Patte <hadrien.patte@datadoghq.com>
2025-01-15 14:38:22 +0100

    Add debug symbols
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

--- .gitlab/build-image.sh
+++ .gitlab/build-image.sh
@@ -7,43 +7,43 @@ TARGET="${TARGET:-}"
 BUILD_ARGS=""
 IFS=$'\n'
 for arg in $DOCKER_BUILD_ARGS; do
-  BUILD_ARGS+=" $(echo "--build-arg $arg")"
+    BUILD_ARGS+=" $(echo "--build-arg $arg")"
 done
 IFS=$' '
 
 # Construct the image tag
 IMAGE_TAG="$CI_COMMIT_TAG"
 if [ "$TARGET" = "debug" ]; then
-  IMAGE_TAG="${IMAGE_TAG}-debug"
+    IMAGE_TAG="${IMAGE_TAG}-debug"
 fi
 IMAGE_REF="registry.ddbuild.io/$IMAGE_NAME:$IMAGE_TAG"
 
 # Find the right Cilium Runtime image to use for the main Cilium image build
 if [ "$IMAGE_NAME" == "cilium" ]; then
-  CILIUM_RUNTIME_IMAGE="registry.ddbuild.io/cilium-runtime:$IMAGE_TAG"
-  BUILD_ARGS+=" --build-arg CILIUM_RUNTIME_IMAGE=$CILIUM_RUNTIME_IMAGE"
+    CILIUM_RUNTIME_IMAGE="registry.ddbuild.io/cilium-runtime:$IMAGE_TAG"
+    BUILD_ARGS+=" --build-arg CILIUM_RUNTIME_IMAGE=$CILIUM_RUNTIME_IMAGE"
 fi
 
 METADATA_FILE=$(mktemp)
 docker buildx build --platform linux/amd64,linux/arm64 \
-  --tag "$IMAGE_REF" \
-  --file "$DOCKERFILE_PATH" \
-  $BUILD_ARGS \
-  --label CILIUM_VERSION="$(cat VERSION)" \
-  --label target=prod \
-  --label CI_PIPELINE_ID="$CI_PIPELINE_ID" \
-  --label CI_JOB_ID="$CI_JOB_ID" \
-  --target "$TARGET" \
-  --push \
-  --metadata-file "$METADATA_FILE" \
-  "$DOCKER_CTX"
+    --tag "$IMAGE_REF" \
+    --file "$DOCKERFILE_PATH" \
+    $BUILD_ARGS \
+    --label CILIUM_VERSION="$(cat VERSION)" \
+    --label target=prod \
+    --label CI_PIPELINE_ID="$CI_PIPELINE_ID" \
+    --label CI_JOB_ID="$CI_JOB_ID" \
+    --target "$TARGET" \
+    --push \
+    --metadata-file "$METADATA_FILE" \
+    "$DOCKER_CTX"
 
 ddsign sign "$IMAGE_REF" --docker-metadata-file "$METADATA_FILE"
 
 # Always build the debug version of the Cilium image
-if [ "$IMAGE_NAME" == "cilium" ]; then
-  METADATA_FILE_DEBUG=$(mktemp)
-  docker buildx build --platform linux/amd64,linux/arm64 \
+# if [ "$IMAGE_NAME" == "cilium" ]; then
+METADATA_FILE_DEBUG=$(mktemp)
+docker buildx build --platform linux/amd64,linux/arm64 \
     --tag "$IMAGE_REF"-debug \
     --file "$DOCKERFILE_PATH" \
     $BUILD_ARGS \
@@ -55,5 +55,5 @@ if [ "$IMAGE_NAME" == "cilium" ]; then
     --push \
     --metadata-file "$METADATA_FILE_DEBUG" \
     "$DOCKER_CTX"
-  ddsign sign "$IMAGE_REF"-debug --docker-metadata-file "$METADATA_FILE_DEBUG"
-fi
+ddsign sign "$IMAGE_REF"-debug --docker-metadata-file "$METADATA_FILE_DEBUG"
+# fi
