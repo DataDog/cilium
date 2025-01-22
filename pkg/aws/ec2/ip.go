af9ffc825e356b567412c7dc5c6d1446221f4264
Hadrien Patte <hadrien.patte@datadoghq.com>
2025-01-22 17:46:01 +0100

    Add logs
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

---
+++ pkg/aws/ec2/ip.go
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package ec2
+
+import (
+	"errors"
+	"net/netip"
+
+	"github.com/cilium/cilium/pkg/ipam/option"
+)
+
+type (
+	PrefixUsage []bool
+	//PrefixesUsage map[netip.Prefix]PrefixUsage
+)
+
+var prefixesUsage = make(map[netip.Prefix]PrefixUsage)
+
+// ENIPDPrefix returns the /28 prefix for a given IP address
+func ENIPDPrefix(addr netip.Addr) (netip.Prefix, error) {
+	return addr.Prefix(option.ENIPDBlockBitsSizeIPv4)
+}
+
+func IndexInPrefix(addr netip.Addr, prefix netip.Prefix) int {
+	return int(addr.AsSlice()[3] - prefix.Addr().AsSlice()[3])
+}
+
+func AddrInPrefix(prefix netip.Prefix, index int) netip.Addr {
+	addr := prefix.Addr()
+	for i := 0; i < index; i++ {
+		addr = addr.Next()
+	}
+	return addr
+}
+
+func AssignedIP(ip string) error {
+	addr, err := netip.ParseAddr(ip)
+	if err != nil {
+		return err
+	}
+	prefix, err := ENIPDPrefix(addr)
+	if err != nil {
+		return err
+	}
+	index := IndexInPrefix(addr, prefix)
+	if index >= option.ENIPDBlockSizeIPv4 {
+		logHADRIEN.Errorf("Index %d too big for IP %s in prefix %s", index, addr, prefix)
+		return nil
+	}
+	prefixUsage, ok := prefixesUsage[prefix]
+	if !ok {
+		prefixUsage = make([]bool, option.ENIPDBlockSizeIPv4)
+	}
+	prefixUsage[index] = true
+	prefixesUsage[prefix] = prefixUsage
+	logHADRIEN.WithField("PrefixesUsage", prefixesUsage).Infof("Assigned IP %s from prefix %s", addr, prefix)
+	return nil
+}
+
+func AssignedIPs(ips []string) error {
+	var errs []error
+	for _, ip := range ips {
+		if err := AssignedIP(ip); err != nil {
+			errs = append(errs, err)
+			continue
+		}
+	}
+	return errors.Join(errs...)
+}
+
+func UnassignedIP(ip string) error {
+	addr, err := netip.ParseAddr(ip)
+	if err != nil {
+		return err
+	}
+	prefix, err := ENIPDPrefix(addr)
+	if err != nil {
+		return err
+	}
+	index := IndexInPrefix(addr, prefix)
+	prefixUsage, ok := prefixesUsage[prefix]
+	if !ok {
+		prefixUsage = make([]bool, option.ENIPDBlockSizeIPv4)
+	}
+	prefixUsage[index] = false
+	prefixesUsage[prefix] = prefixUsage
+	logHADRIEN.WithField("PrefixesUsage", prefixesUsage).Infof("Unassigned IP %s from prefix %s", addr, prefix)
+	return nil
+}
+
+func UnassignedIPs(ips []string) error {
+	var errs []error
+	for _, ip := range ips {
+		if err := UnassignedIP(ip); err != nil {
+			errs = append(errs, err)
+			continue
+		}
+	}
+	return errors.Join(errs...)
+}
+
+func FindIP() (string, bool) {
+	for prefix, prefixUsage := range prefixesUsage {
+		for index, used := range prefixUsage {
+			if !used {
+				addr := AddrInPrefix(prefix, index).String()
+				logHADRIEN.WithField("PrefixesUsage", prefixesUsage).Infof("Found IP %s from prefix %s", addr, prefix)
+				return addr, true
+			}
+		}
+	}
+	return "", false
+}
