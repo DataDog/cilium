f07ebbc28219dca80af93cb801bf6bfa1a95f0e9
Hadrien Patte <hadrien.patte@datadoghq.com>
2025-01-22 17:46:01 +0100

    Add logs
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

---
+++ pkg/aws/denseip/mock.go
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package denseip
+
+import (
+	"net/netip"
+)
+
+type mock struct{}
+
+func NewMock() *mock {
+	return &mock{}
+}
+
+func (_ *mock) FindIPs(subnet netip.Prefix, addressesCount int32) (addresses []string, found bool) {
+	return
+}
+func (_ *mock) RegisterIPsUsed(ips []string, subnet netip.Prefix) {}
+func (_ *mock) RegisterIPsUnused(ips []string)                    {}
