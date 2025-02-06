52712c496ee24aada08563a86853fe2631a0b980
Hadrien Patte <hadrien.patte@datadoghq.com>
2025-02-06 17:24:06 +0100

    Fix azure infinite loop
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

--- pkg/azure/api/api.go
+++ pkg/azure/api/api.go
@@ -233,6 +233,12 @@ func (c *Client) listVirtualMachineScaleSetsNetworkInterfaces(ctx context.Contex
 	for _, virtualMachineScaleSet := range virtualMachineScaleSets {
 		virtualMachineScaleSetNetworkInterfaces, err := c.listVirtualMachineScaleSetNetworkInterfaces(ctx, *virtualMachineScaleSet.Name)
 		if err != nil {
+			// For scale set created by AKS node group (otherwise it will return an empty list) without any instances API will return not found. Then it can be skipped.
+			var respErr *azcore.ResponseError
+			if errors.As(err, &respErr) && respErr.StatusCode == http.StatusNotFound {
+				log.WithError(err).WithField("scaleSet", virtualMachineScaleSetName).Errorf("HADRIEN VMSS network interfaces not found")
+				continue
+			}
 			return nil, err
 		}
 
@@ -298,12 +304,6 @@ func (c *Client) listVirtualMachineScaleSetNetworkInterfaces(ctx context.Context
 		nextResult, err := pager.NextPage(ctx)
 
 		if err != nil {
-			// For scale set created by AKS node group (otherwise it will return an empty list) without any instances API will return not found. Then it can be skipped.
-			var respErr *azcore.ResponseError
-			if errors.As(err, &respErr) && respErr.StatusCode == http.StatusNotFound {
-				log.WithError(err).WithField("scaleSet", virtualMachineScaleSetName).Errorf("HADRIEN am I stuck in a loop?")
-				continue
-			}
 			return nil, err
 		}
 
