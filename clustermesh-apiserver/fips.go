f00df0d16832d60ae886417b2fdfc23dac5d5239
Hadrien Patte <hadrien.patte@datadoghq.com>
2025-02-11 14:20:59 +0100

    Make all cilium components FIPS
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

---
+++ clustermesh-apiserver/fips.go
@@ -0,0 +1,5 @@
+package main
+
+// Package fipsonly restricts all TLS configuration to FIPS-approved settings.
+// See https://github.com/golang/go/blob/master/src/crypto/tls/fipsonly/fipsonly.go
+import _ "crypto/tls/fipsonly"
