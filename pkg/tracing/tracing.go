1ee35dc180fce408df3f082b555d9362543e64dc
Anton Ippolitov <anton.ippolitov@datadoghq.com>
2025-03-24 10:45:58 +0100

    more tracing

--- pkg/tracing/tracing.go
+++ pkg/tracing/tracing.go
@@ -10,14 +10,14 @@ import (
 
 var WithError = tracer.WithError
 
-func StartSpan(ctx context.Context) (tracer.Span, context.Context) {
+func StartSpan(ctx context.Context, operationName string) (tracer.Span, context.Context) {
 	// We inspect the callstack to get the name of the calling function
 	programCounters := make([]uintptr, 1)
 	runtime.Callers(2, programCounters)
 	function := runtime.FuncForPC(programCounters[0])
 	return tracer.StartSpanFromContext(
 		ctx,
-		"cilium.TODO",
+		operationName,
 		tracer.ResourceName(strings.Split(function.Name(), ".")[3]),
 		tracer.ServiceName("cilium-operator"),
 	)
