3185d1ee1e965a7bbce9f036e6664e09c1343a4e
Hadrien Patte <hadrien.patte@datadoghq.com>
2025-04-03 20:37:51 +0200

    debug hubble metrics
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

--- pkg/hubble/metrics/api/registry.go
+++ pkg/hubble/metrics/api/registry.go
@@ -73,6 +73,7 @@ func (r *Registry) ValidateAndCreateHandler(registry *prometheus.Registry, metri
 func (r *Registry) validateAndCreateHandlerLocked(registry *prometheus.Registry, metricsConfig *MetricConfig, metricNames *map[string]*MetricConfig) (*NamedHandler, error) {
 	plugin, ok := r.handlers[metricsConfig.Name]
 	if !ok {
+		r.log.Errorf("HADRIEN HERE metricsConfig |%v| metricsConfig.Name |%v| plugin |%v|", metricsConfig, metricsConfig.Name, plugin)
 		return nil, fmt.Errorf("metric '%s' does not exist", metricsConfig.Name)
 	}
 
