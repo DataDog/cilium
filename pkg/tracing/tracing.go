665bd1acb8573040e98546eb86ef046ae7eec2f2
Hadrien Patte <hadrien.patte@datadoghq.com>
2025-05-13 17:52:28 +0200

    Fix panic
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

--- pkg/tracing/tracing.go
+++ pkg/tracing/tracing.go
@@ -1,24 +1,31 @@
 package tracing
 
 import (
-        "context"
-        "runtime"
-        "strings"
+	"context"
+	"fmt"
+	"runtime"
+	"strings"
 
-        "gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
 var WithError = tracer.WithError
 
 func StartSpan(ctx context.Context) (tracer.Span, context.Context) {
-        // We inspect the callstack to get the name of the calling function
-        programCounters := make([]uintptr, 1)
-        runtime.Callers(2, programCounters)
-        function := runtime.FuncForPC(programCounters[0])
-        return tracer.StartSpanFromContext(
-                ctx,
-                "cilium.TODO",
-                tracer.ResourceName(strings.Split(function.Name(), ".")[3]),
-                tracer.ServiceName("cilium-operator"),
-        )
+	// We inspect the callstack to get the name of the calling function
+	programCounters := make([]uintptr, 1)
+	runtime.Callers(2, programCounters)
+	function := runtime.FuncForPC(programCounters[0])
+	resourceName := function.Name()
+	if len(strings.Split(resourceName, ".")) > 3 {
+		resourceName = strings.Split(resourceName, ".")[3]
+	}
+	fmt.Printf("resourceName: %s\n", resourceName)
+
+	return tracer.StartSpanFromContext(
+		ctx,
+		"cilium.TODO",
+		tracer.ResourceName(resourceName),
+		tracer.ServiceName("cilium-operator"),
+	)
 }
