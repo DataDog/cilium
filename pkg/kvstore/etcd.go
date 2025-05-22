633cbc88910c512dd5943da8cfbef12f07751ba3
Hadrien Patte <hadrien.patte@datadoghq.com>
2025-05-22 15:52:26 +0200

    Debug etcd
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

--- pkg/kvstore/etcd.go
+++ pkg/kvstore/etcd.go
@@ -559,6 +559,8 @@ func connectEtcdClient(ctx context.Context, config *client.Config, cfgPath strin
 		logger: log.WithFields(logrus.Fields{
 			"endpoints": config.Endpoints,
 			"config":    cfgPath,
+			//"X_config":  config,
+			"X_opts": opts,
 		}),
 	}
 
@@ -585,7 +587,7 @@ func connectEtcdClient(ctx context.Context, config *client.Config, cfgPath strin
 		ParallelRequests: clientOptions.MaxInflight,
 	}, ciliumratemetrics.APILimiterObserver())
 
-	ec.logger.Info("Connecting to etcd server...")
+	ec.logger.Info("Connecting to etcd server [[[CAN YOU SEE ME????]]]...")
 
 	leaseTTL := option.Config.KVstoreLeaseTTL
 	if option.Config.KVstoreLeaseTTL == 0 {
