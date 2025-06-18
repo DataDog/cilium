b05956e85e73c7c38425ad1057b0a79b76e78879
Hadrien Patte <hadrien.patte@datadoghq.com>
2025-06-18 16:22:48 +0200

    [PATCH] connectivity tests: do not deploy client2 on same node as client (https://github.com/datadog/cilium-cli/pull/13)
    Allow echo-same-node pod to be deployed on a different node
    
    Disable lrp-{client,backend} pods creation
    
    Disable CCNP tests
    
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

--- cilium-cli/connectivity/check/deployment.go
+++ cilium-cli/connectivity/check/deployment.go
@@ -1011,20 +1011,9 @@ func (ct *ConnectivityTest) deploy(ctx context.Context) error {
 			Labels:      map[string]string{"other": "echo"},
 			Annotations: ct.params.DeploymentAnnotations.Match(echoSameNodeDeploymentName),
 			Affinity: &corev1.Affinity{
-				PodAffinity: &corev1.PodAffinity{
-					RequiredDuringSchedulingIgnoredDuringExecution: []corev1.PodAffinityTerm{
-						{
-							LabelSelector: &metav1.LabelSelector{
-								MatchExpressions: []metav1.LabelSelectorRequirement{
-									{Key: "name", Operator: metav1.LabelSelectorOpIn, Values: []string{clientDeploymentName}},
-								},
-							},
-							TopologyKey: corev1.LabelHostname,
-						},
-					},
-				},
 				NodeAffinity: ct.maybeNodeToNodeEncryptionAffinity(),
 			},
+			NodeSelector:   ct.params.NodeSelector,
 			Tolerations:    ct.params.GetTolerations(),
 			ReadinessProbe: newLocalReadinessProbe(containerPort, "/"),
 		}, ct.params.DNSTestServerImage)
@@ -1066,27 +1055,12 @@ func (ct *ConnectivityTest) deploy(ctx context.Context) error {
 	if err != nil {
 		ct.Logf("✨ [%s] Deploying %s deployment...", ct.clients.src.ClusterName(), client2DeploymentName)
 		clientDeployment := newDeployment(deploymentParameters{
-			Name:        client2DeploymentName,
-			Kind:        kindClientName,
-			Image:       ct.params.CurlImage,
-			Command:     []string{"/usr/bin/pause"},
-			Labels:      map[string]string{"other": "client"},
-			Annotations: ct.params.DeploymentAnnotations.Match(client2DeploymentName),
-			Affinity: &corev1.Affinity{
-				PodAffinity: &corev1.PodAffinity{
-					RequiredDuringSchedulingIgnoredDuringExecution: []corev1.PodAffinityTerm{
-						{
-							LabelSelector: &metav1.LabelSelector{
-								MatchExpressions: []metav1.LabelSelectorRequirement{
-									{Key: "name", Operator: metav1.LabelSelectorOpIn, Values: []string{clientDeploymentName}},
-								},
-							},
-							TopologyKey: corev1.LabelHostname,
-						},
-					},
-				},
-				NodeAffinity: ct.maybeNodeToNodeEncryptionAffinity(),
-			},
+			Name:         client2DeploymentName,
+			Kind:         kindClientName,
+			Image:        ct.params.CurlImage,
+			Command:      []string{"/usr/bin/pause"},
+			Labels:       map[string]string{"other": "client"},
+			Annotations:  ct.params.DeploymentAnnotations.Match(client2DeploymentName),
 			NodeSelector: ct.params.NodeSelector,
 			Tolerations:  ct.params.GetTolerations(),
 		})
@@ -1226,7 +1200,7 @@ func (ct *ConnectivityTest) deploy(ctx context.Context) error {
 							{
 								LabelSelector: &metav1.LabelSelector{
 									MatchExpressions: []metav1.LabelSelectorRequirement{
-										{Key: "name", Operator: metav1.LabelSelectorOpIn, Values: []string{clientDeploymentName}},
+										{Key: "name", Operator: metav1.LabelSelectorOpIn, Values: []string{clientDeploymentName, client2DeploymentName}},
 									},
 								},
 								TopologyKey: corev1.LabelHostname,
