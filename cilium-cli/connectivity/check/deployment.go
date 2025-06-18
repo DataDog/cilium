c093e7af6a42acd774e2931c004e4d457af7d055
Hadrien Patte <hadrien.patte@datadoghq.com>
2025-06-18 16:22:48 +0200

    [PATCH] connectivity tests: do not deploy client2 on same node as client (https://github.com/datadog/cilium-cli/pull/13)
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

--- cilium-cli/connectivity/check/deployment.go
+++ cilium-cli/connectivity/check/deployment.go
@@ -902,27 +902,12 @@ func (ct *ConnectivityTest) deploy(ctx context.Context) error {
 	if err != nil {
 		ct.Logf("✨ [%s] Deploying %s deployment...", ct.clients.src.ClusterName(), client2DeploymentName)
 		clientDeployment := newDeployment(deploymentParameters{
-			Name:        client2DeploymentName,
-			Kind:        kindClientName,
-			Image:       ct.params.CurlImage,
-			Command:     []string{"/usr/bin/pause"},
-			Labels:      map[string]string{"other": "client"},
-			Annotations: ct.params.DeploymentAnnotations.Match(client2DeploymentName),
-			Affinity: &corev1.Affinity{
-				PodAffinity: &corev1.PodAffinity{
-					RequiredDuringSchedulingIgnoredDuringExecution: []corev1.PodAffinityTerm{
-						{
-							LabelSelector: &metav1.LabelSelector{
-								MatchExpressions: []metav1.LabelSelectorRequirement{
-									{Key: "name", Operator: metav1.LabelSelectorOpIn, Values: []string{clientDeploymentName}},
-								},
-							},
-							TopologyKey: corev1.LabelHostname,
-						},
-					},
-				},
-				NodeAffinity: ct.maybeNodeToNodeEncryptionAffinity(),
-			},
+			Name:         client2DeploymentName,
+			Kind:         kindClientName,
+			Image:        ct.params.CurlImage,
+			Command:      []string{"/usr/bin/pause"},
+			Labels:       map[string]string{"other": "client"},
+			Annotations:  ct.params.DeploymentAnnotations.Match(client2DeploymentName),
 			NodeSelector: ct.params.NodeSelector,
 			Tolerations:  ct.params.GetTolerations(),
 		})
@@ -1062,7 +1047,7 @@ func (ct *ConnectivityTest) deploy(ctx context.Context) error {
 							{
 								LabelSelector: &metav1.LabelSelector{
 									MatchExpressions: []metav1.LabelSelectorRequirement{
-										{Key: "name", Operator: metav1.LabelSelectorOpIn, Values: []string{clientDeploymentName}},
+										{Key: "name", Operator: metav1.LabelSelectorOpIn, Values: []string{clientDeploymentName, client2DeploymentName}},
 									},
 								},
 								TopologyKey: corev1.LabelHostname,
