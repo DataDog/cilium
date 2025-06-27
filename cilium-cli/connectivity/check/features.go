e611ba1b7b1105d6f878bd4552e1be83e714b4c9
Hadrien Patte <hadrien.patte@datadoghq.com>
2025-06-27 17:21:22 +0200

    Don't loop over every single cilium pod to detect features
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

--- cilium-cli/connectivity/check/features.go
+++ cilium-cli/connectivity/check/features.go
@@ -378,11 +378,11 @@ func (ct *ConnectivityTest) detectFeatures(ctx context.Context) error {
 		// Extract pod-specific features
 		err = ct.extractFeaturesFromRuntimeConfig(ctx, ciliumPod, features)
 		if err != nil {
-			return err
+			continue
 		}
 		err = ct.extractFeaturesFromCiliumStatus(ctx, ciliumPod, features)
 		if err != nil {
-			return err
+			continue
 		}
 		err = ct.extractFeaturesFromUname(ctx, ciliumPod, features)
 		if err != nil {
@@ -399,6 +399,7 @@ func (ct *ConnectivityTest) detectFeatures(ctx context.Context) error {
 			ct.Features = features
 			initialized = true
 		}
+		break
 	}
 
 	ct.ClusterNameLocal = cmp.Or(cm.Data["cluster-name"], "default")
