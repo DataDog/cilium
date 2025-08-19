bcf500861638c73b68fb1b00bea2fe3f4d9c73eb
Anton Ippolitov <anton.ippolitov@datadoghq.com>
2025-08-19 10:58:05 +0200

    log

--- bpf/bpf_lxc.c
+++ bpf/bpf_lxc.c
@@ -66,6 +66,14 @@
 # define ENABLE_PER_PACKET_LB 1
 #endif
 
+static long (*bpf_trace_printk)(const char *fmt, __u32 fmt_size, ...) = (void *) 6;
+#define bpf_printk(fmt, ...)				\
+({							\
+	static const char ____fmt[] = fmt;				\
+	bpf_trace_printk(____fmt, sizeof(____fmt),	\
+			 ##__VA_ARGS__);		\
+})
+
 #ifdef ENABLE_PER_PACKET_LB
 
 #ifdef ENABLE_IPV4
@@ -1289,13 +1297,20 @@ skip_vtep:
 	if (is_defined(ENABLE_HOST_ROUTING)) {
 		int oif = 0;
 
+		bpf_printk("fib_redirect_v4: calling with daddr=%pI4", &ip4->daddr);
 		ret = fib_redirect_v4(ctx, ETH_HLEN, ip4, false, false, ext_err, &oif);
-		if (ret == BPF_FIB_LKUP_RET_NOT_FWDED)
+		bpf_printk("fib_redirect_v4: returned ret=%d, oif=%d", ret, oif);
+		
+		if (ret == BPF_FIB_LKUP_RET_NOT_FWDED) {
+			bpf_printk("fib_redirect_v4: BPF_FIB_LKUP_RET_NOT_FWDED, going to stack");
 			goto pass_to_stack;
-		if (fib_ok(ret))
+		}
+		if (fib_ok(ret)) {
+			bpf_printk("fib_redirect_v4: success, redirecting to network");
 			send_trace_notify(ctx, TRACE_TO_NETWORK, SECLABEL_IPV4,
 					  *dst_sec_identity, TRACE_EP_ID_UNKNOWN, oif,
 					  trace.reason, trace.monitor);
+		}
 		return ret;
 	}
 
