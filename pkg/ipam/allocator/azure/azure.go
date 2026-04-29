8cce31a58d8c736f2f0616a06c522ffa9aaf8193
jaredledvina <jared.ledvina@datadoghq.com>
2026-04-29 13:57:40 -0400

    [azure] Plumb AzureReleaseExcessIPs into NewNodeManager
    Signed-off-by: jaredledvina <jared.ledvina@datadoghq.com>

--- pkg/ipam/allocator/azure/azure.go
+++ pkg/ipam/allocator/azure/azure.go
@@ -21,6 +21,7 @@ type AllocatorAzure struct {
 	AzureResourceGroup          string
 	AzureUserAssignedIdentityID string
 	AzureUsePrimaryAddress      bool
+	AzureReleaseExcessIPs       bool
 	ParallelAllocWorkers        int64
 	LimitIPAMAPIBurst           int
 	LimitIPAMAPIQPS             float64
@@ -73,7 +74,7 @@ func (a *AllocatorAzure) Start(ctx context.Context, getterUpdater ipam.CiliumNod
 		return nil, fmt.Errorf("unable to create Azure client: %w", err)
 	}
 	instances := azureIPAM.NewInstancesManager(a.rootLogger, azureClient)
-	nodeManager, err := ipam.NewNodeManager(a.logger, instances, getterUpdater, iMetrics, a.ParallelAllocWorkers, false, 0, false)
+	nodeManager, err := ipam.NewNodeManager(a.logger, instances, getterUpdater, iMetrics, a.ParallelAllocWorkers, a.AzureReleaseExcessIPs, 0, false)
 	if err != nil {
 		return nil, fmt.Errorf("unable to initialize Azure node manager: %w", err)
 	}
