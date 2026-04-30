46b68c6943bd724a749d4e127836df02c4bcb0f3
jaredledvina <jared.ledvina@datadoghq.com>
2026-04-30 12:59:12 -0400

    [azure] Cache IPConfiguration name to skip GET on VMSS unassign
    Signed-off-by: jaredledvina <jared.ledvina@datadoghq.com>

--- pkg/azure/api/mock/mock.go
+++ pkg/azure/api/mock/mock.go
@@ -267,6 +267,7 @@ func (a *API) AssignPrivateIpAddressesVMSS(ctx context.Context, vmName, vmssName
 				IP:     ip.String(),
 				Subnet: subnetID,
 				State:  types.StateSucceeded,
+				Name:   "Cilium-mock-" + ip.String(),
 			})
 		}
 
@@ -290,7 +291,7 @@ func (a *API) UnassignPrivateIpAddressesVM(ctx context.Context, interfaceName st
 	return nil
 }
 
-func (a *API) UnassignPrivateIpAddressesVMSS(ctx context.Context, vmName, vmssName, interfaceName string, addresses []string) error {
+func (a *API) UnassignPrivateIpAddressesVMSS(ctx context.Context, vmName, vmssName, interfaceName string, ipConfigNames []string) error {
 	a.rateLimit()
 	a.delaySim.Delay(UnassignPrivateIpAddressesVMSS)
 
@@ -301,13 +302,13 @@ func (a *API) UnassignPrivateIpAddressesVMSS(ctx context.Context, vmName, vmssNa
 		return err
 	}
 
-	if len(addresses) == 0 {
+	if len(ipConfigNames) == 0 {
 		return nil
 	}
 
-	releaseSet := make(map[string]struct{}, len(addresses))
-	for _, ip := range addresses {
-		releaseSet[ip] = struct{}{}
+	dropNames := make(map[string]struct{}, len(ipConfigNames))
+	for _, name := range ipConfigNames {
+		dropNames[name] = struct{}{}
 	}
 
 	foundInterface := false
@@ -324,7 +325,7 @@ func (a *API) UnassignPrivateIpAddressesVMSS(ctx context.Context, vmName, vmssNa
 
 		kept := intf.Addresses[:0]
 		for _, addr := range intf.Addresses {
-			if _, drop := releaseSet[addr.IP]; drop {
+			if _, drop := dropNames[addr.Name]; drop {
 				if s, ok := a.subnets[addr.Subnet]; ok {
 					_, ipNet, err := net.ParseCIDR(addr.IP + "/32")
 					if err == nil {
