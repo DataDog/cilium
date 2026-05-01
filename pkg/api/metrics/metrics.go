17bc3e43a6030dcb036de6d008c74b5d1750d659
jaredledvina <jared.ledvina@datadoghq.com>
2026-05-01 16:24:46 -0400

    metrics: Increase available api_duration_seconds metric buckets
    Signed-off-by: jaredledvina <jared.ledvina@datadoghq.com>

--- pkg/api/metrics/metrics.go
+++ pkg/api/metrics/metrics.go
@@ -25,7 +25,7 @@ func New(subsystem string) *Metrics {
 			Name:      "api_duration_seconds",
 			Help:      "Duration of interactions with API",
 			Buckets: []float64{0.005, 0.025, 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1.0, 1.25, 1.5, 2, 3,
-				4, 5, 6, 8, 10, 15, 20, 30, 45, 60},
+				4, 5, 6, 8, 10, 15, 20, 30, 45, 60, 90, 120, 300, 600},
 		}, []string{"operation", "response_code"}),
 
 		RateLimit: metric.NewHistogramVec(metric.HistogramOpts{
